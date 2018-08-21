13b0870baa17a4dc12f624c414c5bed67d9b4c07
jurajkusnier <jurajkusnier@gmail.com>
2018-08-21 18:51:36 +0200

    Static reference to Application Context added

--- app/src/main/java/com/jurajkusnier/googletasks/App.kt
+++ app/src/main/java/com/jurajkusnier/googletasks/App.kt
@@ -1,7 +1,19 @@
 package com.jurajkusnier.googletasks
 
 import android.app.Application
+import android.content.Context
 
 class App: Application() {
+
+    init {
+        instance = this
+    }
+
+    companion object {
+        private lateinit var instance:App
+
+        val applicationContext: Context
+            get() = instance.applicationContext
+    }
 
 }
